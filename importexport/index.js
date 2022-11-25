f39d19437420fb96a311bb8430eecfc4e82a63d5
vikas148 <vb334531@gmail.com>
2022-11-25 23:18:45 +0530

    index

---
+++ importexport/index.js
@@ -0,0 +1,15 @@
+// const add = require('./opr')  //to import a single fxn
+// const opr = require('./opr');
+const {add, sub, mult,name} = require('./opr');
+
+// console.log(opr);
+// console.log(opr.add(5,5));
+// console.log(opr.sub(5,5));
+
+// console.log(add(5,5));
+ 
+
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(mult(5,5));
+console.log(name);
