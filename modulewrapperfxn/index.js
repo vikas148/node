3f2c965f8e747426c612d74fe11752c27cf9516b
vikas148 <vb334531@gmail.com>
2022-11-26 23:36:09 +0530

    index

---
+++ modulewrapperfxn/index.js
@@ -0,0 +1,16 @@
+// constname ="vikas";
+// console.log(name); 
+//mtlb ki node ye pura kaam krta hai  hme ese nhi likhna hota hai fxn bna ke
+
+(function (exports,require,module,_filename,_dirname) {
+    constname ="vikas";
+    console.log(name);
+    
+    module.exports = {}
+})
+
+console.log(__dirname);
+console.log(__filename);
+
+//IFEE function
+
