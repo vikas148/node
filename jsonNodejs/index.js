0658f252819148709336ec2980fe7df3c146632a
vikas148 <vb334531@gmail.com>
2022-11-27 22:20:25 +0530

    index

---
+++ jsonNodejs/index.js
@@ -0,0 +1,24 @@
+const bioData={
+    name: "vikas",
+    age:22,
+    channel: "livechillkonje",
+}
+
+// console.log(bioData.name);
+
+// //to convert an object to json file
+// const jsondata = JSON.stringify(bioData);
+// console.log(jsondata);
+// // console.log(jsondata.name);  //not allowed in json
+
+// //to conver json into string
+// const orgdata = JSON.parse(jsondata)
+// console.log(orgdata);
+
+// console.log(orgdata.name); //valid in strings
+
+
+
+
+
+
